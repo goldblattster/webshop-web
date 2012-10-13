6a0ace5836ecc1afbb45fd407ddf4d2465faf525
Ariel Goldblatt <a.k.a.gldbltt@gmail.com>
2012-10-12 20:15:49 -0700

    imported css and scripts

--- app/views/layouts/default.html.php
+++ app/views/layouts/default.html.php
@@ -1,31 +1,29 @@
-<?php
-/**
- * Lithium: the most rad php framework
- *
- * @copyright     Copyright 2012, Union of RAD (http://union-of-rad.org)
- * @license       http://opensource.org/licenses/bsd-license.php The BSD License
- */
-?>
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
 <head>
-	<?php echo $this->html->charset();?>
-	<title>Application &gt; <?php echo $this->title(); ?></title>
-	<?php echo $this->html->style(array('debug', 'lithium')); ?>
-	<?php echo $this->scripts(); ?>
-	<?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
+  <?php echo $this->html->charset();?>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Application &gt; <?php echo $this->title(); ?></title>
+  <?php echo $this->html->style(array('http://fonts.googleapis.com/css?family=Titillium+Web|Open+Sans', 'normalize', 'foundation', 'main')); ?>
+  <?php echo $this->scripts(); ?>
+  <?php echo $this->html->link('Icon', null, array('type' => 'icon')); ?>
 </head>
 <body class="app">
-	<div id="container">
-		<div id="header">
-			<h1>Application</h1>
-			<h2>
-				Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
-			</h2>
-		</div>
-		<div id="content">
-			<?php echo $this->content(); ?>
-		</div>
-	</div>
+<div id="container">
+  <div id="header">
+    <h1>Application</h1>
+
+    <h2>
+      Powered by <?php echo $this->html->link('Lithium', 'http://lithify.me/'); ?>.
+    </h2>
+  </div>
+  <div id="content">
+    <?php echo $this->content(); ?>
+  </div>
+</div>
 </body>
 </html>
